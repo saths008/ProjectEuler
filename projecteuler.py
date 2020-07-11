8c8973ef053fd03ffc2a8f16e38ec2a3b0465ac1
saths008 <63187492+saths008@users.noreply.github.com>
2020-07-11 19:18:58 +0100

    Update projecteuler.py

--- projecteuler.py
+++ projecteuler.py
@@ -1,6 +1,7 @@
+#multiples of 3 and 5
 numbers = list(range(1,1000))
 newList = []
-for x in numbers:
+for x in numbers:  #if remainder equals 0, it is divisible
     if x % 5 == 0:
         newList.append(x)
     if x % 3 == 0:
